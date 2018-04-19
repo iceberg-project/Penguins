a2f9887f824367fbcf2a6cb7735b6161c9d16a6a
Hieu M Le <hle@login.cm.cluster>
2018-04-19 19:50:34 -0400

    add code to split test/train

--- split_train_test.py
+++ split_train_test.py
@@ -4,40 +4,27 @@ import os.path
 from m_util import *
 opt = argparse.ArgumentParser().parse_args()
 #opt.im_fold = '/nfs/bigbox/hieule/penguin_data/CROPPED/p500'
-opt.im_fold = '/nfs/bigbox/hieule/penguin_data/MB_Same_Size/Train/Train_all/CROPPED/p500'
+opt.im_fold = '/nfs/bigbox/hieule/penguin_data/p1000'
 opt.train_fold = opt.im_fold+'_train/'
 opt.test_fold = opt.im_fold+'_test/'
-sdmkdir(opt.test_fold+'A')
-sdmkdir(opt.test_fold+'B')
-sdmkdir(opt.test_fold+'tif')
-sdmkdir(opt.train_fold+'A')
-sdmkdir(opt.train_fold+'B')
-sdmkdir(opt.train_fold+'tif')
 
 imlist = []
+train_ratio = 0.7
+
 for root,_,fnames in sorted(os.walk(opt.im_fold+'/A')):
     for fname in fnames:
         if fname.endswith('.png') :
             imlist.append(fname)
-
 print imlist
-test_list_short = ["WV02_20160218213346_1030010053BC0300_16FEB18213346-M1BS-500638407020_01_P002_u08rf3031.png",
-        "WV03_20160121015319_104001001762AC00_16JAN21015319-M1BS-500638671020_01_P001_u08rf3031.png",
-        "WV03_20160202130111_1040010017D54900_16FEB02130111-M1BS-500638661010_01_P001_u08rf3031.png",
-        "orthoWV02_11FEB111306175-M1BS-10300100080D2800_u08rfAEAC.png"
-        ]
-test_list = []
-for i in test_list_short:
-    test_list.append(i)
-    test_list.append(i.replace('M1BS','P1BS'))
-print test_list
-for im in imlist:
-    if im not in test_list:
-        copyfile(opt.im_fold+'/A/'+im,opt.train_fold+'A/'+im)
-        copyfile(opt.im_fold+'/B/'+im,opt.train_fold+'B/'+im)
-        copyfile(opt.im_fold+'/tif/'+im.replace('.png','.tif'),opt.train_fold+'tif/'+im.replace('.png','.tif'))
-
-    if im in test_list:
-        copyfile(opt.im_fold+'/A/'+im,opt.test_fold+'A/'+im)
-        copyfile(opt.im_fold+'/B/'+im,opt.test_fold+'B/'+im)
-        copyfile(opt.im_fold+'/tif/'+im.replace('.png','.tif'),opt.test_fold+'tif/'+im.replace('.png','.tif'))
+nim = len(imlist)
+print nim
+nim_train = int(float(nim)*train_ratio)
+print nim_train
+idx = np.random.permutation(nim)
+train_list = imlist[idx[0:nim_train]]
+print train_list
+#test_list_short = ["WV02_20160218213346_1030010053BC0300_16FEB18213346-M1BS-500638407020_01_P002_u08rf3031.png",
+#        "WV03_20160121015319_104001001762AC00_16JAN21015319-M1BS-500638671020_01_P001_u08rf3031.png",
+#        "WV03_20160202130111_1040010017D54900_16FEB02130111-M1BS-500638661010_01_P001_u08rf3031.png",
+#        "orthoWV02_11FEB111306175-M1BS-10300100080D2800_u08rfAEAC.png"
+#        ]
