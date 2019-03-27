394a7654f60ed873ae3cc58e3e7faf6eceaca970
bentocg <bentocollares@gmail.com>
2019-03-27 12:17:00 -0300

    debugging shapes again

--- train_model.py
+++ train_model.py
@@ -122,6 +122,9 @@ def train_model(model, dataloader, criterion_seg, criterion_reg, optimizer, sche
                     is_mask = torch.Tensor([ele for ele in is_mask])
                     is_mask = is_mask.to(torch.float32)
 
+                    print(is_mask.shape)
+                    print(target_img.shape)
+
                     if use_gpu:
                         input_img, target_img, area, is_mask = input_img.cuda(), target_img.cuda(), area.cuda(), is_mask.cuda()
 
@@ -180,8 +183,7 @@ def train_model(model, dataloader, criterion_seg, criterion_reg, optimizer, sche
                         is_mask = is_mask.reshape([len(is_mask), 1, 1, 1])
                         is_mask = is_mask.to(torch.float32)
 
-                        print(is_mask.shape)
-                        print(target_img.shape)
+
 
                         # cuda
                         if use_gpu:
