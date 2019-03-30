fixed bug with normalizing groundtruth masks

utils/dataloaders/transforms.py
@@ -1,6 +1,7 @@
 import torchvision.transforms.functional as TF
 from torchvision import transforms
 import numpy as np
+import torch
 
 __all__ = ['TransformPair']
 
@@ -66,8 +67,9 @@ class TransformPair(object):
             ground_truth = center_crop(ground_truth)
 
         # change locations to tensor
-        area = np.sum(np.array(ground_truth) > 0).astype(np.float32)
-        ground_truth = TF.normalize(TF.to_tensor(ground_truth), [0.5], [0.25])
+        ground_truth = (np.array(ground_truth) > 0).astype(np.float32)
+        area = np.sum(ground_truth)
+        ground_truth = torch.Tensor(ground_truth)
         image = TF.normalize(TF.to_tensor(image), [0.5] * 3, [0.25] * 3)
 
         return image, ground_truth, area