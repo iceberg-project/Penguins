1d4a00bce20c3512b8807ec7d6fb395093c52c41
bentocg <bentocollares@gmail.com>
2019-03-31 02:18:53 -0300

    full length training

--- train_model.py
+++ train_model.py
@@ -156,7 +156,7 @@ def train_model(model, dataloader, criterion_seg, criterion_reg, optimizer, sche
                         loss_reg = criterion_reg(pred_area, area)
 
                         # save stats
-                        if iter > 0 and iter %10 == 0:
+                        if iter > 0 and iter % 10 == 0:
                             writer.add_scalar(f"training loss area {loss_name.split('-')[-1]}", loss_reg,
                                               global_step)
                             writer.add_scalar(f"training loss mask {loss_name.split('-')[0]}", loss_seg,
@@ -164,7 +164,7 @@ def train_model(model, dataloader, criterion_seg, criterion_reg, optimizer, sche
                             writer.add_scalar("learning rate", optimizer.param_groups[-1]['lr'], global_step)
 
                         # backprop
-                        if epoch > 1:
+                        if epoch > 2:
                             loss_total = loss_reg + loss_seg
                         else:
                             loss_total = loss_reg
